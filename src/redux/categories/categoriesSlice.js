6726090c396ea631d13771d85ceb6ce6dbefca63
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2023-08-21 16:29:00 -0400

    Bookstore: add reducers and actions

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -7,10 +7,9 @@ export const categoriesSlice = createSlice({
   initialeState,
   reducers: {
     checkStatus: (state, action) => {
-      state.categories =
-        action.payload === "Under construction"
-          ? "Under construction"
-          : state.categories
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
     },
   },
 });
